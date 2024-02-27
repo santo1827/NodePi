aab35a15dc1d513c70edcdbb549c772bc83eb4a8
santo1827 <136288759+santo1827@users.noreply.github.com>
2024-02-27 16:23:01 +1000

    remove endpoint

--- index.js
+++ index.js
@@ -12,8 +12,5 @@ app.get('/hello', (req, res) => {
     res.json("Hello everyone!!!")
 })
 
-app.get('/test', (req, res) => {
-    res.json('This is so cool!!')
-})
 
 app.listen(port, () => console.log(`Server listening on Port: ${port}`))
